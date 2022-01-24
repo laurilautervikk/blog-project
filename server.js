3e7bcbc1ed036471bb7c3a068f3795f7af09ebab
Lauri Lauter <lauri.lauter@vikk.ee>
2022-01-24 14:13:04 +0200

    Server working

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer(() => {
-    res.statuscode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('hello!');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-type", "text/plain");
+  res.end("hello!");
 });
 
-server.listen(3000,() => {
-    console.log('Server started on http://localhost:3000');
+server.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
 });
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.end('hello world!');
+});
+
+server.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
