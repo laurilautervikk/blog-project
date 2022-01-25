eefc20138e9a9c0aca036627c4d46b4a2c5c3ded
Lauri Lauter <lauri.lauter@vikk.ee>
2022-01-25 14:52:11 +0200

    Still working

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,16 @@
-'empty';
+import express from 'express';
+import fs from 'fs';
+import createUser from './user/createUser';
+import getUser from './user/getUser';
+import getUsers from './user/getUsers';
+const router = express.Router();
+var userRoutes: string[] = [];
+
+// fs.readdirSync(__dirname + '/user').forEach(function (file) {
+//   userRoutes.push(file.substr(0, file.indexOf('.')));
+// });
+
+console.log(userRoutes.toString());
+router.use('/users', [createUser, getUser, getUsers]);
+
+export default router;
