7ed83df6672a635eb8970aff526ab34a76f76475
Lauri Lauter <lauri.lauter@vikk.ee>
2022-01-25 10:05:27 +0200

    Types changed

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,27 +1,37 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm';
 
 @Entity()
-class User {
+export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  @Column()
+  @Column('varchar', { length: 150 })
   firstName: string;
-  @Column()
+  @Column('varchar', { length: 150 })
   midlleName: string;
-  @Column()
+  @Column('varchar', { length: 150 })
   lastName: string;
-  @Column()
+  @Column('varchar')
   mobile: string;
-  @Column()
+  @Column('varchar', { length: 320, unique: true })
   email: string;
   @Column()
   registeredAt: Date;
   @Column()
   lastLogin: Date;
-  @Column()
+  @Column('tinytext', { nullable: true })
   intro: string;
-  @Column()
+  @Column('text', { nullable: true })
   profile: string;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
 
 export default User;
